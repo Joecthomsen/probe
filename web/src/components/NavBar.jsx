2eeba81778266d2f1a1e07573c87fe141cf4015d
joecthomsen <joecthomsen@gmail.com>
2022-09-09 18:34:53 +0200

    Removed link at homepage

--- web/src/components/NavBar.jsx
+++ web/src/components/NavBar.jsx
@@ -1,13 +1,16 @@
-import {Link} from 'react-router-dom'
+//import {Link} from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <nav className="nav-bar-container">
             <h1 className="nav-bar-logo">PROBE</h1>
             <div className="nav-bar-links-container">
-                <Link to='/' className="nav-bar-links">Home</Link>
+                <h1>Hej</h1>
+                <h1>About</h1>
+                <h1>Login</h1>
+                {/* <Link to='/' className="nav-bar-links">Home</Link>
                 <Link to='/about' className="nav-bar-links">About</Link>
-                <Link to='/login' className="nav-bar-links">Login</Link>
+                <Link to='/login' className="nav-bar-links">Login</Link> */}
             </div>
         </nav>
     );
