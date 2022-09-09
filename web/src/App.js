205b561f7799475142b786fecfe4625b8db33bb6
joecthomsen <joecthomsen@gmail.com>
2022-09-09 19:28:50 +0200

    BrowserRoute switched with HashRouts

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,21 @@
 import Homepage from './pages/Home';
-// import About from './pages/About';
-// import Login from './pages/Login';
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
+import About from './pages/About';
+import Login from './pages/Login';
+import {
+  HashRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <Homepage/>
-    // <BrowserRouter>
-    //   <Routes>
-    //       <Route path='/' element={<Homepage/>}/>
-    //       <Route path='/about' element={<About/>}/>
-    //       <Route path='/login' element={<Login/>}/>
-    //   </Routes>
-    // </BrowserRouter>
+    <HashRouter>
+      <Routes>
+          <Route path='/' element={<Homepage/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </HashRouter>
     // <div className="App">
     //   <LandingPage />
     // </div>
