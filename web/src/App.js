843bdfbf463bcd7a8a6c8e4de179c5a5b3a2aa29
joecthomsen <joecthomsen@gmail.com>
2022-09-13 14:11:43 +0200

    404 page added

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import Homepage from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
 import UserProfile from "./pages/UserProfile";
+import PageNotFound from './pages/PageNotFound';
 
 import {
   HashRouter,
@@ -17,6 +18,7 @@ function App() {
           <Route path='/about' element={<About/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/userProfile' element={<UserProfile/>} />
+          <Route path='*' element={<PageNotFound/>}/>
       </Routes>
     </HashRouter>
     // <div className="App">
