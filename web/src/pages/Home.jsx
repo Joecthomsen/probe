20d2d63808f6e9783d715b7b7892404de0b0ddb1
joecthomsen <joecthomsen@gmail.com>
2022-09-13 20:17:16 +0200

    Carousel refactored

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,72 +1,26 @@
-import LandingPageStudyCard from "../components/StudyCard";
 import "../styles/homepage.css"
-import cardData from "../api/clinical_trial_api_mock"
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-
-const responsive = {
-    superLargeDesktop: {
-        breakpoint: { max: 4000, min: 3000 },
-        items: 5
-      },
-      desktop: {
-        breakpoint: { max: 3000, min: 1600 },
-        items: 4,
-        partialVisibilityGutter: 40
-      },
-      tablet: {
-        breakpoint: { max: 1599, min: 1124 },
-        items: 3
-      },
-      smallTablet: {
-        breakpoint: {max: 1123, min: 724},
-        items: 2
-      },
-      mobile: {
-        breakpoint: { max: 723, min: 0 },
-        items: 1
-      }
-};
+import CustomCarousel from "../components/Carousel";
 
 const LandingPage = () => {
 
-    const studies = cardData.clinicalTrials;
-
-    const cardList = studies.map((element, index) => {
-        return(<LandingPageStudyCard key={index} 
-            header={element.header} 
-            title={element.title} 
-            country={element.county}
-            city={element.city} 
-            description={element.cardDescription}/>)
-    })
-
     return ( 
         <div>
-                <main className="bg-image">
-                    <div>
-                        <div className="color-overlay"/>
-                        <div className="header">
-                            <h1>Clinical trial matching</h1>
-                            <h2>World wide experimental medicin initiateves</h2>
-                        </div>
-                    </div>                   
-                </main> 
-                <div className="card-page">
-                    <div className="card-header">
-                        <h1>Selection of Studies</h1>
+            <main className="bg-image">
+                <div>
+                    <div className="color-overlay"/>
+                    <div className="header">
+                        <h1>Clinical trial matching</h1>
+                        <h2>World wide experimental medicin initiateves</h2>
                     </div>
-                    <Carousel 
-                        responsive={responsive} 
-                        infinite={true} 
-                        containerClass="carusel-container" 
-                        itemClass="carusel-item"
-                        showDots={true}
-                        partialVisible={false}
-                        >
-                        {cardList}
-                    </Carousel>
-                </div> 
+                </div>                   
+            </main> 
+            <div className="card-page">
+                <div className="card-header">
+                    <h1>Selection of Studies</h1>
+                </div>
+                <CustomCarousel/>
+            </div> 
         </div>
     )
 }
