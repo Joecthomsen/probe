e1b168b4370ca9bca719af4c958e3ea8f77d5141
joecthomsen <joecthomsen@gmail.com>
2022-09-14 22:50:50 +0200

    Added create user subpage

--- web/src/pages/Login.jsx
+++ web/src/pages/Login.jsx
@@ -2,14 +2,16 @@ import "../styles/login.css"
 import Login from "../components/Login";
 
 const login = () => {
-    return (
+    return (    
         <div className="page-container">
             <Login/>
             <div className="create-new-user">
+                {/* <div className="color-overlay" /> */}
+                <h1 className="create-user-header">New Here?</h1>
+                <h2>Sign up and and discover a new world of opportunities!</h2>
+                <button>Create New User</button>
             </div>
         </div> 
-
     );
 }
- 
 export default login;
