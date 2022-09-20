f208a34c8c3feb54cb1266e4e7b660148c0ce5b9
joecthomsen <joecthomsen@gmail.com>
2022-09-20 16:58:29 +0200

    second submission form added + navigator

--- web/src/components/CreateNewUserFormOne.jsx
+++ web/src/components/CreateNewUserFormOne.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import users from "../api/user_api_mock";
+import { useNavigate } from 'react-router-dom';
 
 const CreateUserFormOne = () => {
 
@@ -12,11 +13,13 @@ const CreateUserFormOne = () => {
     const [city, setCity] = useState();
     const [region, setRegion] = useState();
     const [country, setCountry] = useState();
+    const [gender, setGender] = useState();
 
     const [error, setError] = useState(false)
     const [errorMsg, setErrorMsg] = useState("")
-    const [firstForm, setFirstForm] = useState(true)
+    const [firstForm, setFirstForm] = useState(false)
 
+    const navigate = useNavigate();
 
     const handleEmail = (event) => {
         setEmail(event.target.value)
@@ -45,8 +48,12 @@ const CreateUserFormOne = () => {
     const handleCountry = (event) => {
         setCountry(event.target.value)
     }
+    const handleGender = (event) => {
+        console.log(event.target.value)
+        setGender(event.target.value)
+    }
 
-    const checkForm = (event) => {
+    const checkFirstForm = (event) => {
         event.preventDefault();
         let computedErrorMsg = "Please fill in the folloving information: "
         let hasError = false;
@@ -112,23 +119,38 @@ const CreateUserFormOne = () => {
         }
     }
 
-    const submitUser = () => {
+    function checkSecondForm()  {
+        let hasError = false
+        if(!gender){
+            setError(true);
+            setErrorMsg("Please select gender")
+            return false;
+        }
+        return true;
+    }
 
-        users.users.push({
-            "id": 0,
-            "image": "<PATH_TO_IMAGE_HERE>",
-            "firstName": firstName,
-            "lastName": lastName,
-            "cpr": "NIL",
-            "age": 40,
-            "chronicDisease": "Lung cancer stage 4",
-            "streetName": "Nørrebrogade",
-            "doorNumber": 42,
-            "zipCode": 2200,
-            "city": city,
-            "region": region,
-            "county": country
-        })
+
+    const submitUser = (event) => {
+
+        event.preventDefault();
+        if(checkSecondForm()){
+            navigate("/")
+            users.users.push({
+                "id": 0,
+                "image": "<PATH_TO_IMAGE_HERE>",
+                "firstName": firstName,
+                "lastName": lastName,
+                "cpr": "NIL",
+                "age": 40,
+                "chronicDisease": "Lung cancer stage 4",
+                "streetName": "Nørrebrogade",
+                "doorNumber": 42,
+                "zipCode": 2200,
+                "city": city,
+                "region": region,
+                "county": country
+            })
+        }
     }
 
 return (
@@ -146,17 +168,37 @@ return (
             <input onChange={handleCity} type="text" placeholder="city"/>
             <input onChange={handleRegion} type="text" placeholder="Region"/>
             <input onChange={handleCountry} type="text" placeholder="County"/>
-            <button onClick={checkForm} >Next</button>
+            <button onClick={checkFirstForm} >Next</button>
             {error ? <p className="error-messages">{errorMsg}</p> : <p></p>}
         </form>
     : 
-        <form className="create-user-form">
-            <label htmlFor="Create a new user"></label>
-            <input type="text" placeholder="Your diagnose (If any)"></input>
-            <input placeholder="Discribe your life with your diagnose"></input>
-            <button onClick={submitUser} >Create Users</button>
-            {error ? <p className="error-messages">{errorMsg}</p> : <p></p>}
-        </form>
+        <div>
+            <div className="gender-selection">
+                <p>Select gender</p>
+                <div className="radio-buttons">
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" onChange={handleGender} name="exampleRadios" id="exampleRadios1" value="male" />
+                        <label className="form-check-label" htmlFor="exampleRadios1">
+                            Male
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" onChange={handleGender} name="exampleRadios" id="exampleRadios2" value="female"/>
+                        <label className="form-check-label" htmlFor="exampleRadios2">
+                            Female
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <form className="create-user-form">        
+                <label htmlFor="Create a new user"></label>
+                <input type="text" placeholder="Your diagnose (If any)"></input>
+                <input type="text" placeholder="Age whe you got diagnosed (If appliable)"></input> 
+                <input type="text" placeholder="Occupation"></input> 
+                <button onClick={submitUser} >Create Users</button>
+                {error ? <p className="error-messages">{errorMsg}</p> : <p></p>}
+            </form>
+        </div>
     );
 }
  
