a86776386f03350b3ead6ed74bf0fcce10167a3c
joecthomsen <joecthomsen@gmail.com>
2022-09-22 16:46:30 +0200

    Added globe on landingpage

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import "../styles/homepage.css"
 import "react-multi-carousel/lib/styles.css";
 import CustomCarousel from "../components/Carousel";
+import world from "../resources/map-of-the-world-png.png"
 
 const LandingPage = () => {
 
     return ( 
         <div>
             <main className="bg-image">
-                <div>
-                    <div className="color-overlay"/>
+                <div className="landingpage-wrapper">
+                    {/* <div className="color-overlay"/> */}
                     <div className="header">
                         <h1>Clinical trial matching</h1>
                         <h2>World wide experimental medicin initiateves</h2>
                     </div>
+                    <img className="world" src={world} alt="null" />
                 </div>                   
             </main> 
             <div className="card-page">
