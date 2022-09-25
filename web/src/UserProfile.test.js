ea72c5c39629dcde9670cd95c9919e7b40434bb7
Kasper Buur at Rokoko <kasper.buur@rokoko.com>
2022-09-25 17:45:27 +0200

    fixed userprofile test

--- web/src/UserProfile.test.js
+++ web/src/UserProfile.test.js
@@ -4,6 +4,7 @@ import { render, screen} from "@testing-library/react";
 import UserProfile from "./pages/UserProfile";
 import {userPreferences} from "./stores/UserPreferencesStore";
 import ActivateDeactivateButton from "./components/UserPreferences/ActivateDeactivateButton";
+import PreferenceArray from "./components/UserPreferences/PreferenceArray";
 
 
 test('correct Header', () => {
@@ -12,7 +13,7 @@ test('correct Header', () => {
     expect(header).toHaveTextContent("User Preferences")
 });
 
-test('correct button', () => {
+test('correct button in activateDeactivateButton', () => {
     render(<ActivateDeactivateButton />)
     const button = screen.queryByRole('button');
     if (userPreferences.active) {
@@ -24,6 +25,35 @@ test('correct button', () => {
 
 })
 
-test("preferenceArray", () => {
+test("preferenceArray has correct length", () => {
+    render(<PreferenceArray />);
+
+    userPreferences.setCurrentPref("pref1");
+    userPreferences.setCurrentChoice("choice1");
+    userPreferences.addPreference();
+    userPreferences.setCurrentPref("pref2");
+    userPreferences.setCurrentChoice("choice2");
+    userPreferences.addPreference();
+
+    expect(userPreferences.thisUsersPreferences.length).toBe(2);
+
+
+})
+
+test("preferenceArray has twice as many buttons as elements", () => {
+    render(<PreferenceArray />);
+
+    userPreferences.setCurrentPref("pref1");
+    userPreferences.setCurrentChoice("choice1");
+    userPreferences.addPreference();
+    userPreferences.setCurrentPref("pref2");
+    userPreferences.setCurrentChoice("choice2");
+    userPreferences.addPreference();
+
+    const buttons = screen.queryAllByRole('button');
+    expect(buttons.length).toBe(4);
+
+
+
 
 })
