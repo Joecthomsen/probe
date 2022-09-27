4b5c1f3917107b2e8167c4bfffa889016b3d3c6d
joecthomsen <joecthomsen@gmail.com>
2022-09-27 15:29:23 +0200

    added dependencies

--- src/main/java/com/probe/App.java
+++ src/main/java/com/probe/App.java
@@ -1,13 +1,33 @@
 package com.probe;
 
+import java.io.File;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+    public static void main(String[] args) {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir("temp");
+        String port = System.getenv("DevOpsPort");
+        port = port !=null ? port:"8080";
+     
+        tomcat.setPort(Integer.parseInt(port));
+        tomcat.getConnector();
+        tomcat.addWebapp("",new File("src/main/webapp").getAbsolutePath());
+     
+        try {
+            tomcat.start();
+            tomcat.getServer().await();
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+        }
+     
+     }
+     
 }
