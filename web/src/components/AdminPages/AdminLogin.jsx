7dcc66383a9fd819c9703469ca71f67fe0394b85
Annika Ehlers <e_hlers@live.dk>
2022-10-02 11:50:57 +0200

    AdminLogin

--- web/src/components/AdminPages/AdminLogin.jsx
+++ web/src/components/AdminPages/AdminLogin.jsx
@@ -1,25 +1,35 @@
-import {TextField, Button} from "@mui/material";
+import { TextField, Button } from "@mui/material";
+import React, { useState, useEffect } from "react";
 
-export default function Login() {
+function Login() {
+  const [err, setErr] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const login = (event) => {
+    setRedirect = true;
+  }
+
   return (
     <form>
-        <TextField
-          style={{ width: "200px", margin: "5px" }}
-          type="text"
-          label="Username"
-          variant="outlined"
-        />
-        <br />
-        <TextField
-          style={{ width: "200px", margin: "5px" }}
-          type="password"
-          label="Password"
-          variant="outlined"
-        />
-        <br/>
-        <Button variant="contained" color="primary" disabled='true'>
-          save
-        </Button>
+      <TextField
+        style={{ width: "200px", margin: "5px" }}
+        type="text"
+        label="Username"
+        variant="outlined"
+      />
+      <br />
+      <TextField
+        style={{ width: "200px", margin: "5px" }}
+        type="password"
+        label="Password"
+        variant="outlined"
+      />
+      <br />
+      <Button variant="contained" color="primary" disabled="true" onClick={login}>
+        save
+      </Button>
     </form>
   );
-};
+}
+
+export default Login;
