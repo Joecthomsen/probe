db20cb7dafab582bf45e7a24486dda877b41a04f
joecthomsen <joecthomsen@gmail.com>
2022-10-02 18:35:37 +0200

    Removed ekstra create user page

--- web/src/App.js
+++ web/src/App.js
@@ -27,8 +27,7 @@ function App() {
               <Route path='/login' element={<Layout> <Login/> </Layout>}/>
               <Route path='/userProfile' element={<Layout> <UserProfile/> </Layout>} />
               <Route path='/edittrials' element={<Layout><EditTrials/></Layout>}/>
-              <Route path='/createuser' element={ <Layout> <CreateUser/> </Layout> }/>
-              <Route path='/createuser' element={<CreateUser/>}/>
+              <Route path='/createuser' element={ <Layout> <CreateUser/></Layout> }/>
               <Route path='/trials' element={<ViewTrials/>}/>
               <Route path='/admin' element={<AdminLogin/>}/>
               <Route path='/admin-page' element={<AdmLayout><Page/> </AdmLayout>}/>
