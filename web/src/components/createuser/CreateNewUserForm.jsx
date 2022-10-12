bc077fc557a9c6e654bfe2849e15b000264e5887
joecthomsen <joecthomsen@gmail.com>
2022-10-12 10:39:59 +0200

    User form edit

--- web/src/components/createuser/CreateNewUserForm.jsx
+++ web/src/components/createuser/CreateNewUserForm.jsx
@@ -20,16 +20,16 @@ const CreateUserFormOne = () => {
     }
     const handlePassword = (event) => {
         userStore.setPassword(event.target.value)
-    } 
+    }
     const handleRepeatPassword = (event) => {
         userStore.setRepeatPassword(event.target.value)
-    } 
+    }
     const handleFirstName = (event) => {
         userStore.setFirstName(event.target.value)
-    } 
+    }
     const handleLastName = (event) => {
         userStore.setLastName(event.target.value)
-    } 
+    }
     const handleBirthday = (event) => {
         userStore.setDob(event.target.value)
     }
@@ -46,29 +46,35 @@ const CreateUserFormOne = () => {
         userStore.setGender(event.target.value)
     }
     const handleDiagnose = (event) => {
-        userStore.setDiagnose(event.target.value)
+        userStore.setChronicDisease(event.target.value)
     }
-    const handleOccupation = (event) => {
-        userStore.setOccupation(event.target.value)
+    const handleWeight = (event) => {
+        userStore.setWeight(event.target.value)
     }
+    const handlePhoneNumber = (event) => {
+        userStore.setPhoneNumber(event.target.value)
+    }
+    // const handleOccupation = (event) => {
+    //     userStore.setOccupation(event.target.value)
+    // }
     // const handleDescription = (event) => {
     //     userStore.setDescription(event.target.value)
     // }
-    const handleAgeWhenDiagnosed = (event) => {
-        userStore.ageWhenDiagnosed(event.target.value)
-    }
+    // const handleAgeWhenDiagnosed = (event) => {
+    //     userStore.ageWhenDiagnosed(event.target.value)
+    // }
 
     const checkFirstForm = (event) => {
 
         console.log(userStore.firstName)
- 
+
         event.preventDefault();
         let computedErrorMsg = "Please fill in the folloving information: "
         let hasError = false;
 
         if(userStore.password !== userStore.repeatPassword){
             hasError = true
-            computedErrorMsg = "Password does not match"          
+            computedErrorMsg = "Password does not match"
         }
         else{
             if(!userStore.email){
@@ -136,65 +142,67 @@ const CreateUserFormOne = () => {
     //     return true;
     // }
 
+    //'http://localhost:8080/user/add'
+    //'https://probe.joecthomsen.dk/user/add'
     const submitUser = (event) => {
         event.preventDefault();
         console.log("submitting........")
         axios.post('https://probe.joecthomsen.dk/user/add', userStore.getUserObject())
             .then(response => {console.log(response)})
             .catch(error => console.log(error))
-        }
+    }
 
-return (
-    //Check if the user wants to create a medical user
-    !userStore.createMedicalUser
-    ?       
-        firstForm 
-        ?             
-            <form className="create-user-form">
-                <SelectUserType/>
-                <label htmlFor="Create a new user"></label>
-                <input onChange={handleEmail} name="email" type="text" placeholder={userStore.email ? userStore.email : "Email"}/>
-                <input onChange={handlePassword} type="password" placeholder="Password"/>
-                <input onChange={handleRepeatPassword} type="password" placeholder="Repeat password"/>
-                <input onChange={handleFirstName} type="text" placeholder={userStore.firstName ? userStore.firstName : "First name(s)"}/>
-                <input onChange={handleLastName} type="text" placeholder={userStore.lastName ? userStore.lastName : "Last name"}/>
-                <label htmlFor="user-birthday">Day of Birth:</label>
-                <input onChange={handleBirthday} type="date" name="Birthday" id="user-birthday" placeholder={userStore.dob ? userStore.dob : ""}/>
-                <input onChange={handleCity} type="text" placeholder={userStore.city ? userStore.city : "city"}/>
-                <input onChange={handleRegion} type="text" placeholder={userStore.region ? userStore.region : "Region"}/>
-                <input onChange={handleCountry} type="text" placeholder={userStore.country ? userStore.city : "County"}/>
-                <button onClick={checkFirstForm} >Next</button>
-                {userStore.error ? <p className="error-messages">{userStore.errorMsg}</p> : <p></p>}
-            </form>
-        : 
-            <div>
-                <div className="gender-selection">
-                    <p>Select gender</p>
-                    <div className="radio-buttons">
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" onChange={handleGender} name="exampleRadios" id="exampleRadios1" value="male" />
-                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                Male
-                            </label>
-                        </div>
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" onChange={handleGender} name="exampleRadios" id="exampleRadios2" value="female"/>
-                            <label className="form-check-label" htmlFor="exampleRadios2">
-                                Female
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                <form className="create-user-form">        
+    return (
+        //Check if the user wants to create a medical user
+        !userStore.createMedicalUser
+            ?
+            firstForm
+                ?
+                <form className="create-user-form">
+                    <SelectUserType/>
                     <label htmlFor="Create a new user"></label>
-                    <input onChange={handleDiagnose} type="text" placeholder="Your diagnose (If any)"></input>
-                    <input onChange={handleAgeWhenDiagnosed} type="text" placeholder="Age whe you got diagnosed (If appliable)"></input> 
-                    <input onChange={handleOccupation} type="text" placeholder="Occupation"></input> 
-                    <button onClick={submitUser} >Create Users</button>
+                    <input onChange={handleEmail} name="email" type="text" placeholder={userStore.email ? userStore.email : "Email"}/>
+                    <input onChange={handlePassword} type="password" placeholder="Password"/>
+                    <input onChange={handleRepeatPassword} type="password" placeholder="Repeat password"/>
+                    <input onChange={handleFirstName} type="text" placeholder={userStore.firstName ? userStore.firstName : "First name(s)"}/>
+                    <input onChange={handleLastName} type="text" placeholder={userStore.lastName ? userStore.lastName : "Last name"}/>
+                    <label htmlFor="user-birthday">Day of Birth:</label>
+                    <input onChange={handleBirthday} type="date" name="Birthday" id="user-birthday" placeholder={userStore.dob ? userStore.dob : ""}/>
+                    <input onChange={handleCity} type="text" placeholder={userStore.city ? userStore.city : "city"}/>
+                    <input onChange={handleRegion} type="text" placeholder={userStore.region ? userStore.region : "Region"}/>
+                    <input onChange={handleCountry} type="text" placeholder={userStore.country ? userStore.city : "County"}/>
+                    <button onClick={checkFirstForm} >Next</button>
                     {userStore.error ? <p className="error-messages">{userStore.errorMsg}</p> : <p></p>}
                 </form>
-            </div>
-    : //If the user wants to create a medical user
+                :
+                <div>
+                    <div className="gender-selection">
+                        <p>Select gender</p>
+                        <div className="radio-buttons">
+                            <div className="form-check">
+                                <input className="form-check-input" type="radio" onChange={handleGender} name="exampleRadios" id="exampleRadios1" value="male" />
+                                <label className="form-check-label" htmlFor="exampleRadios1">
+                                    Male
+                                </label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="radio" onChange={handleGender} name="exampleRadios" id="exampleRadios2" value="female"/>
+                                <label className="form-check-label" htmlFor="exampleRadios2">
+                                    Female
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <form className="create-user-form">
+                        <label htmlFor="Create a new user"></label>
+                        <input onChange={handleDiagnose} type="text" placeholder="Your diagnose (If any)"></input>
+                        <input onChange={handleWeight} type="text" placeholder="Weight"></input>
+                        <input onChange={handlePhoneNumber} type="text" placeholder="Phone number"></input>
+                        <button onClick={submitUser} >Create Users</button>
+                        {userStore.error ? <p className="error-messages">{userStore.errorMsg}</p> : <p></p>}
+                    </form>
+                </div>
+            : //If the user wants to create a medical user
             <form className="create-user-form">
                 <SelectUserType/>
                 <label htmlFor="Create a new user"></label>
@@ -209,7 +217,7 @@ return (
                 <button onClick={checkFirstForm} >Create User</button>
                 {userStore.error ? <p className="error-messages">{userStore.errorMsg}</p> : <p></p>}
             </form>
-        );
+    );
 }
- 
+
 export default observer(CreateUserFormOne)
