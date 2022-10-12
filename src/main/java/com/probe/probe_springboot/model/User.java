bb959ac28e7d8300df9e2ad44e204743f2fd56d5
joecthomsen <joecthomsen@gmail.com>
2022-10-12 14:04:24 +0200

    Changed DB deployment strategy from INDENTYFY to UPDATE

--- src/main/java/com/probe/probe_springboot/model/User.java
+++ src/main/java/com/probe/probe_springboot/model/User.java
@@ -17,7 +17,7 @@ import java.sql.Date;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private boolean medicalUser;
     private String sex;
