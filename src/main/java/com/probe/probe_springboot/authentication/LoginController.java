7dde9f007e7ed087925d98db2109b369b87a04ae
joecthomsen <joecthomsen@gmail.com>
2022-10-26 20:51:26 +0200

    Backend autorization restored

--- src/main/java/com/probe/probe_springboot/authentication/LoginController.java
+++ src/main/java/com/probe/probe_springboot/authentication/LoginController.java
@@ -9,14 +9,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/authentication" )
 @CrossOrigin(maxAge = 3600)
 public class LoginController {
-
     @Autowired
     private LoginService loginService;
 
-    @PostMapping("/post")
+    @PostMapping("/login")
     public String login(@RequestBody LoginData loginData) throws NotAuthorizedException, JsonProcessingException {
         System.out.println("From controller: " + loginData);
-        return loginService.postLoginData(loginData);
+        return loginService.login(loginData);
     }
 
     @PostMapping("/validate/{token}")
