f5b3d15c4217189b88d02316d48ac4ee68bbd398
Joecthomsen <75262116+Joecthomsen@users.noreply.github.com>
2022-10-26 23:01:26 +0200

    Update LoginController.java

--- src/main/java/com/probe/probe_springboot/authentication/LoginController.java
+++ src/main/java/com/probe/probe_springboot/authentication/LoginController.java
@@ -5,23 +5,23 @@ import com.probe.probe_springboot.exceptions.NotAuthorizedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/authentication" )
-@CrossOrigin(origins = "*")
 public class LoginController {
 
     @Autowired
     private LoginService loginService;
 
-    @PostMapping("/post")
     @CrossOrigin(origins = "*")
+    @PostMapping("/post")
     public String login(@RequestBody LoginData loginData) throws NotAuthorizedException, JsonProcessingException {
         System.out.println("From controller: " + loginData);
         return loginService.postLoginData(loginData);
     }
-
-    @PostMapping("/validate/{token}")
+    
     @CrossOrigin(origins = "*")
+    @PostMapping("/validate/{token}")
         public ValidationData validateToken(@PathVariable String token){
         return loginService.validateToken(token);
     }
