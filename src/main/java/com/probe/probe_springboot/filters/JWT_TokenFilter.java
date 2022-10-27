b93bd0ed7d14ad3e0f4c78ca61a98cd2ce55398b
joecthomsen <joecthomsen@gmail.com>
2022-10-27 21:21:55 +0200

    Updated CORS settings

--- src/main/java/com/probe/probe_springboot/filters/JWT_TokenFilter.java
+++ src/main/java/com/probe/probe_springboot/filters/JWT_TokenFilter.java
@@ -19,14 +19,14 @@ public class JWT_TokenFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 //        @Autowired
         HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-
-        res.setHeader("Access-Control-Allow-Origin", res.getHeader("Origin"));
-        //res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        res.setHeader("Access-Control-Max-Age", "3600");
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+//        HttpServletResponse res = (HttpServletResponse) response;
+//
+//        res.setHeader("Access-Control-Allow-Origin", res.getHeader("Origin"));
+//        //res.setHeader("Access-Control-Allow-Origin", "*");
+//        res.setHeader("Access-Control-Allow-Credentials", "true");
+//        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+//        res.setHeader("Access-Control-Max-Age", "3600");
+//        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
 
         String key = req.getHeader("user");
         String requestEmail = req.getHeader("email");
