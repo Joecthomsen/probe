a4d303f01fab938ad52d3b48d5dd53d989c761af
joecthomsen <joecthomsen@gmail.com>
2022-10-27 18:44:36 +0200

    Try Cors modification on class lavel

--- src/main/java/com/probe/probe_springboot/controller/UserController.java
+++ src/main/java/com/probe/probe_springboot/controller/UserController.java
@@ -12,9 +12,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/user" )
-@CrossOrigin
 public class UserController {
 
     @Autowired
