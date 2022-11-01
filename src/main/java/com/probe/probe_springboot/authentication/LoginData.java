ec6bf616c61a3b28cded98f7b2f0f2c26dfecfcc
joecthomsen <joecthomsen@gmail.com>
2022-11-01 10:30:03 +0100

    Fixed mvn test error

--- src/main/java/com/probe/probe_springboot/authentication/LoginData.java
+++ src/main/java/com/probe/probe_springboot/authentication/LoginData.java
@@ -1,8 +1,11 @@
 package com.probe.probe_springboot.authentication;
 
+import com.probe.probe_springboot.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -11,6 +14,7 @@ public class LoginData {
 
     public LoginData(String email){
         this.email = email;
+
     }
     private String email;
     private String password;
