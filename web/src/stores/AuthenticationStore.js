f3ed1a08450275d80592a013ac1f9c3a0b4897f9
joecthomsen <joecthomsen@gmail.com>
2022-11-01 20:53:08 +0100

    Fixed unused imports

--- web/src/stores/AuthenticationStore.js
+++ web/src/stores/AuthenticationStore.js
@@ -1,14 +1,5 @@
 import {makeAutoObservable} from "mobx"
 import {loginRequest} from '../requests/loginRequest'
-import {useNavigate} from "react-router-dom";
-//import axios from "axios"
-// import jwt_decode from 'jwt-decode';
-// import {EditTrialStoreOBJ} from "./EditTrialStore";
-//import app from "../App";
-
-
-// const getToken = "https://probe.joecthomsen.dk/authentication/jwt";
-//const getToken = "http://localhost:8080/authentication/jwt";
 
 class AuthenticationStore {
 
