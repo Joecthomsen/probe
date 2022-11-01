4c43be195f48b8ccd86f0c035106c43727585e85
kasperBuurDk <kasperbuur@gmail.com>
2022-11-01 15:39:13 +0100

    added a service test

--- src/main/java/com/probe/probe_springboot/repositories/UserPreferences/AUserPreferenceRepository.java
+++ src/main/java/com/probe/probe_springboot/repositories/UserPreferences/AUserPreferenceRepository.java
@@ -1,8 +1,10 @@
 package com.probe.probe_springboot.repositories.UserPreferences;
 import com.probe.probe_springboot.model.UserPreferences.AUserPreference;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 public interface AUserPreferenceRepository extends JpaRepository<AUserPreference, String> {
-    AUserPreference findPreferenceByID(Long ID);
+
+    AUserPreference findByID(@NonNull Long ID);
 
 }
--- src/main/java/com/probe/probe_springboot/service/UserPreferenceService.java
+++ src/main/java/com/probe/probe_springboot/service/UserPreferenceService.java
@@ -1,10 +1,12 @@
 package com.probe.probe_springboot.service;
 
-
+import com.probe.probe_springboot.model.UserPreferences.AUserPreference;
 import com.probe.probe_springboot.model.UserPreferences.UserPreferencesModel;
 import com.probe.probe_springboot.repositories.UserPreferences.UserPreferencesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserPreferenceService {
@@ -19,5 +21,9 @@ public class UserPreferenceService {
             e.printStackTrace();
             throw e;
         }
+    }
+
+    public List<AUserPreference> getUsersPreferences(String ownerMail) {
+        return userPreferencesRepository.findByOwnerMail(ownerMail).getMyPreferences();
     }
 }
