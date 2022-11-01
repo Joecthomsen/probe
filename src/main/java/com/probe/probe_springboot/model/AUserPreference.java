8be49d2510c7f225843f4f966a4bb4e149091155
kasperBuurDk <kasperbuur@gmail.com>
2022-11-01 11:19:04 +0100

    added UserPreferences -Model,repository,service. Currently empty placeholders

---
+++ src/main/java/com/probe/probe_springboot/model/AUserPreference.java
@@ -0,0 +1,25 @@
+package com.probe.probe_springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class AUserPreference {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ID;
+    private String preferenceName;
+    private String choice;
+}
