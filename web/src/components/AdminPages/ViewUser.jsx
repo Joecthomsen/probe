2018051fee0927c481719e5e6f31072dcf2d07a8
Bitten <e_hlers@live.dk>
2022-11-04 17:04:38 +0100

    Show data from mock api

--- web/src/components/AdminPages/ViewUser.jsx
+++ web/src/components/AdminPages/ViewUser.jsx
@@ -3,6 +3,12 @@ import { TextField, Box } from "@mui/material";
 import { useParams } from "react-router-dom";
 import users from "../../api/user_api_mock";
 
+function findUser(id) {
+  return users["users"].find((obj) => {
+    return obj.id == id;
+  });
+}
+
 function TextFields({ data }) {
   return (
     <TextField
@@ -18,9 +24,7 @@ function TextFields({ data }) {
 function ViewUser() {
   let { id } = useParams();
 
-  const user = users.find((obj) => {
-    return obj.id === id;
-  });
+  const user = findUser(id);
   console.log(user);
 
   return (
@@ -40,61 +44,61 @@ function ViewUser() {
               <tr>
                 <td>First Name</td>
                 <td>
-                  <TextFields data="hej" />
+                  <TextFields data={user.firstName} />
                 </td>
               </tr>
               <tr>
                 <td>Last Name</td>
                 <td>
-                  <TextFields data="hej" />
+                  <TextFields data={user.lastName} />
                 </td>
               </tr>
               <tr>
                 <td>CPR</td>
                 <td>
-                  <TextFields data="hej" />
+                  <TextFields data={user.cpr} />
                 </td>
               </tr>
               <tr>
                 <td>Age</td>
                 <td>
-                  <TextFields data="hej" />
+                  <TextFields data={user.age} />
                 </td>
               </tr>
               <tr>
                 <td>Chronic Disease</td>
                 <td>
-                  <TextFields data="hej" />
+                  <TextFields data={user.chronicDisease} />
                 </td>
               </tr>
               <tr>
                 <td>Adress</td>
                 <td>
-                  <TextFields data="hej" />
+                  <TextFields data={user.streetName} />
                 </td>
               </tr>
               <tr>
                 <td>City</td>
                 <td>
-                  <TextFields data="hej" />
+                  <TextFields data={user.city} />
                 </td>
               </tr>
               <tr>
                 <td>Zip Code</td>
                 <td>
-                  <TextFields data="hej" />
+                  <TextFields data={user.zipCode} />
                 </td>
               </tr>
               <tr>
                 <td>Region</td>
                 <td>
-                  <TextFields data="hej" />
+                  <TextFields data={user.region} />
                 </td>
               </tr>
               <tr>
                 <td>Country</td>
                 <td>
-                  <TextFields data="hej" />
+                  <TextFields data={user.county} />
                 </td>
               </tr>
             </tbody>
