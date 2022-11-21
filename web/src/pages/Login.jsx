08a1a5ddffeb9dfdebecd70601fd14cbcc8d6720
joecthomsen <joecthomsen@gmail.com>
2022-11-21 18:52:46 +0100

    Login loading added

--- web/src/pages/Login.jsx
+++ web/src/pages/Login.jsx
@@ -1,13 +1,18 @@
 import "../styles/login.css"
 import Login from "../components/sign_in/Login";
 import CreateNewUser from "../components/sign_in/CreateNewUser";
+import {authenticationStore} from "../stores/AuthenticationStore";
 
 const login = () => {
-    return (    
-        <div className="page-container">
-            <Login/>
-            <CreateNewUser/>
-        </div> 
+    return (
+        authenticationStore.getLoading()
+        ?
+            <h1>LOADING...</h1>
+        :
+            <div className="page-container">
+                <Login/>
+                <CreateNewUser/>
+            </div>
     );
 }
 export default login;
