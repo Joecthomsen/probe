690b2764dac30a617fa910cebaba3a2fa0f99e2b
joecthomsen <joecthomsen@gmail.com>
2022-11-21 19:37:51 +0100

    Removed unused imports

--- web/src/stores/AuthenticationStore.js
+++ web/src/stores/AuthenticationStore.js
@@ -1,4 +1,4 @@
-import {makeAutoObservable, makeObservable} from "mobx"
+import {makeAutoObservable} from "mobx"
 import {loginRequest} from '../requests/loginRequest'
 
 class AuthenticationStore {
