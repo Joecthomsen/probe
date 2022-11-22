bf756c14412ea9f83c811ae37cdb115608a05521
Annika Ehlers <e_hlers@live.dk>
2022-11-22 10:56:43 +0100

    Update flow for deleting user

--- web/src/components/AdminPages/NavBar.jsx
+++ web/src/components/AdminPages/NavBar.jsx
@@ -1,21 +1,13 @@
 import { observer } from "mobx-react-lite";
-import { windowSizeStore } from "../../stores/WindowSizeStore";
 
-const NavBar = () => {
-  function handleResize() {
-    windowSizeStore.setWindowWidth(window.innerWidth);
-    windowSizeStore.setWindowHeight(window.innerHeight);
-  }
-  window.addEventListener("resize", handleResize);
-
-  return windowSizeStore.windowWidth > 950 ? (
-    <nav className="nav-bar-container">
-      <h1 className="nav-bar-logo">PROBE</h1>
-    </nav>
-  ) : (
+const NavBar = ({isLoggedIn}) => {
+  return (
     <nav className="nav-bar-container">
       <h1 className="nav-bar-logo">PROBE</h1>
       <div className="nav-bar-links-container"></div>
+      <button>
+        Log out
+      </button>
     </nav>
   );
 };
