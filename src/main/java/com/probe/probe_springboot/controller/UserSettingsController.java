eb5ee12fdd9b105c3e38e633eaceff6d96b5f5e7
kasperBuurRokoko <kasper.buur@rokoko.com>
2022-11-29 00:09:23 +0100

    fixed error

--- src/main/java/com/probe/probe_springboot/controller/UserSettingsController.java
+++ src/main/java/com/probe/probe_springboot/controller/UserSettingsController.java
@@ -2,29 +2,29 @@ package com.probe.probe_springboot.controller;
 
 
 import com.probe.probe_springboot.model.UserPreferences.UserSettings;
-import com.probe.probe_springboot.service.UserPrefChoiceService;
+import com.probe.probe_springboot.service.UserSettingsService;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping(value = "/userPrefChoice" )
-public class UserPrefChoiceController {
+public class UserSettingsController {
 
-    private final UserPrefChoiceService userPrefChoiceService;
+    private final UserSettingsService userSettingsService;
 
-    public UserPrefChoiceController(UserPrefChoiceService userPrefChoiceService) {
-        this.userPrefChoiceService = userPrefChoiceService;
+    public UserSettingsController(UserSettingsService userSettingsService) {
+        this.userSettingsService = userSettingsService;
     }
 
 
     @GetMapping("/getById/{id}")
     public UserSettings GetUSersPrefChoicesById(@PathVariable String id) {
-        return userPrefChoiceService.getById(id);
+        return userSettingsService.getUserSettingsByOwnID(id);
     }
 
-    @GetMapping("/getByOwnerMail/{id}")
+    @GetMapping("/getByOwnerMail/{ownerMail}")
     public UserSettings GetUSersPrefChoicesByOwnerMail(@PathVariable String ownerMail) {
-        return userPrefChoiceService.getByOwnerMail(ownerMail);
+        return userSettingsService.getUsersSetttingsByOwnersMail(ownerMail);
     }
 
 
